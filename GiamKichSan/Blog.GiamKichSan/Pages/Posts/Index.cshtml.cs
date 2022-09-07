a97a094d4791ffb71383a91af137f72b893a7b75
LDAP\vcduan <duanvc2811@gmail.com>
2022-09-07 11:02:39 +0700

    Add index

--- GiamKichSan/Blog.GiamKichSan/Pages/Posts/Index.cshtml.cs
+++ GiamKichSan/Blog.GiamKichSan/Pages/Posts/Index.cshtml.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.GiamKichSan.Data;
+using Blog.GiamKichSan.Model;
+using Blog.GiamKichSan.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +12,16 @@ namespace Blog.GiamKichSan.Pages.Posts
 {
     public class IndexModel : PageModel
     {
+        public List<PostModel> postsModel { get; set; }
+        private PostServices postServices { get; set; }
+        public IndexModel(BlogDbContext context)
+        {
+            Common.CntGlobal.blogDbContext = context;
+            postServices = new PostServices();
+        }
         public void OnGet()
         {
+            postsModel = postServices.GetAll();
         }
     }
 }
