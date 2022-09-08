21af4f51ec10ca217bfdc4ea9492acd90bd52524
LDAP\vcduan <duanvc2811@gmail.com>
2022-09-08 14:31:17 +0700

    edit

---
+++ GiamKichSan/Blog.GiamKichSan/wwwroot/controller/postUpdate.js
@@ -0,0 +1,27 @@
+﻿let editorPostDetail = document.querySelector('#PostDetailName');
+let editorClass = document.querySelector('main .toolbar-container');
+let $postDetail = $('input[name=PostDetailsDescription]');
+
+
+
+$(document).ready(function () {
+    editorPostDetail.addEventListener('blur', function () {
+        let array = [];
+        editorPostDetail.childNodes.forEach(function (item) {
+            array.push(item.innerHTML);
+        });
+        $postDetail[0].value = JSON.stringify(array);
+    });
+    DecoupledEditor
+        .create(editorPostDetail, {
+            // toolbar: [ 'heading', '|', 'bold', 'italic', 'link' ]
+        })
+        .then(editor => {
+            editorClass.prepend(editor.ui.view.toolbar.element);
+            window.editor = editor;
+            editorPostDetail.focus();
+        })
+        .catch(err => {
+            console.log(err.stack);
+        });
+});
