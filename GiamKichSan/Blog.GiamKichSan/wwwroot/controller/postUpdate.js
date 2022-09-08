fd885817166dff8b6442a676f9def614b7cd89b6
LDAP\vcduan <duanvc2811@gmail.com>
2022-09-08 18:23:10 +0700

    edit

--- GiamKichSan/Blog.GiamKichSan/wwwroot/controller/postUpdate.js
+++ GiamKichSan/Blog.GiamKichSan/wwwroot/controller/postUpdate.js
@@ -2,14 +2,52 @@
 let editorClass = document.querySelector('main .toolbar-container');
 let $postDetail = $('input[name=PostDetailsDescription]');
 
+let getEditor = {
+    startIndex: 1,
+    maxText: 200,
+    convertObjToArrayNode: function (control, iDParent) {
+        let arrayChild = [];
+        if (control == null) return arrayChild;
+        if (control.outerHTML.length > this.maxText) {
+            let controlHTML = this.getHTMLByControl(control);
+            let iDControlHTML = this.startIndex++;
+            arrayChild.push({ "ID": iDControlHTML, "Control": controlHTML, "IDParent": iDParent });
 
+            for (let index = 0; index < control.children.length; index++) {
+                let childArray = this.convertObjToArrayNode(control.children[index], iDControlHTML);
+                arrayChild.push(...childArray);
+            }
+        }
+        else {
+            arrayChild.push({ "ID": this.startIndex++, "Control": control.outerHTML, "IDParent": iDParent });
+        }
+        return arrayChild;
+    },
+    getHTMLByControl: function (control) {
+        let tagName = control.localName;
+        let attrHTML = ''
+        for (var index = 0; index < control.attributes.length; index++) {
+            attrHTML += ` ${control.attributes[index].localName}="${control.getAttribute(control.attributes[index].localName)}"`;
+        }
+
+        if (this.isChildControl(control))
+            return `<${tagName}${attrHTML}></${tagName}>`;
+        else
+            return `<${tagName}${attrHTML}/>`;
+    },
+    isChildControl: function (control) {
+        return control && control.children && control.children.length > 0;
+    },
+};
 
 $(document).ready(function () {
     editorPostDetail.addEventListener('blur', function () {
         let array = [];
-        editorPostDetail.childNodes.forEach(function (item) {
-            array.push(item.innerHTML);
-        });
+        let childRoot = this.children;
+        for (let index = 0; index < childRoot.length; index++) {
+            let arrChildren = getEditor.convertObjToArrayNode(childRoot[index], 0);
+            array.push(...arrChildren);
+        }
         $postDetail[0].value = JSON.stringify(array);
     });
     DecoupledEditor
